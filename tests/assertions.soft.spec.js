b294d002d750de18c6d3aaf02a66900827a09fdf
NITESH YADAV <nitesh.yadav2009@gmail.com>
2024-11-04 18:00:40 +0530

    archived tests git hub actions poc

--- tests/assertions.soft.spec.js
+++
@@ -1,9 +0,0 @@
-const { expect, test } = require("@playwright/test");
-
-test("assertions", async ({ page }) => {
-  await page.goto("https://www.demoblaze.com/");
-  await expect.soft(page).toHaveTitle("STORE");
-  await expect.soft(page).toHaveURL("https://www.demoblaze.com");
-  await expect.soft(page.locator(".navbar-brand")).toBeVisible();
-  page.close();
-});
